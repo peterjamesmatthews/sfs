d9ad58b3342d2d1f84283ddc2f6c7254a7b17b98
Peter Matthews <peter@pjmatthews.dev>
2024-03-10 18:44:07 -0400

    move /server.go to pjm.dev/sfs/cmd/server/main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"pjm.dev/sfs/graph"
 	"pjm.dev/sfs/memdb"
@@ -19,13 +18,9 @@ func main() {
 		port = defaultPort
 	}
 
-	var gqlHandler http.Handler
 	db := memdb.NewSeededDatabase()
-	resolver := graph.Resolver{AuthN: &db, SFS: &db}
-	gqlHandler = handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &resolver}))
-	gqlHandler = db.WrapInAuthentication(gqlHandler)
 
-	http.Handle("/graphql", gqlHandler)
+	http.Handle("/graphql", graph.GetGQLHandler(&db, &db))
 	http.Handle("/", playground.Handler("GraphQL playground", "/graphql"))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
