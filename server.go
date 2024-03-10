cbcb864157389b8c533e98d357cbea8def6f380e
Peter Matthews <peter@pjmatthews.dev>
2024-03-10 10:14:08 -0400

    add server entrypoint

---
+++ server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"pjm.dev/sfs/graph"
+	"pjm.dev/sfs/memdb"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	var gqlHandler http.Handler
+	db := memdb.NewMemDatabase()
+	resolver := graph.Resolver{AuthN: &db, SFS: &db}
+	gqlHandler = handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &resolver}))
+	gqlHandler = db.WrapInAuthentication(gqlHandler)
+
+	http.Handle("/graphql", gqlHandler)
+	http.Handle("/", playground.Handler("GraphQL playground", "/graphql"))
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
