922f068a3536fda30044f3d8488d918b283dd276
Peter Matthews <peter@pjmatthews.dev>
2024-03-10 21:03:19 -0400

    move seeded database to server.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,7 +6,9 @@ import (
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/google/uuid"
 	"pjm.dev/sfs/graph"
+	"pjm.dev/sfs/graph/model"
 	"pjm.dev/sfs/memdb"
 )
 
@@ -18,11 +20,36 @@ func main() {
 		port = defaultPort
 	}
 
-	db := memdb.NewSeededDatabase()
-
-	http.Handle("/graphql", graph.GetGQLHandler(&db, &db))
+	db := newSeededDatabase()
+	http.Handle("/graphql", graph.GetGQLHandler(&db, &db, &db))
 	http.Handle("/", playground.Handler("GraphQL playground", "/graphql"))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func newSeededDatabase() memdb.MemDatabase {
+	peter := &model.User{ID: uuid.NewString(), Name: "Peter"}
+	users := []*model.User{peter}
+
+	root := &model.Folder{}
+
+	foo := &model.Folder{
+		ID:     uuid.NewString(),
+		Name:   "Foo",
+		Owner:  peter,
+		Parent: root,
+	}
+
+	bar := &model.File{
+		ID:      uuid.NewString(),
+		Name:    "Bar",
+		Owner:   peter,
+		Parent:  root,
+		Content: "Hello World!",
+	}
+
+	root.Children = []model.Node{foo, bar}
+
+	return memdb.MemDatabase{Root: root, Users: users}
 }
