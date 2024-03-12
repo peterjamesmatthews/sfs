cfac4c79b96a8461c9df0837205351fd56ee75e1
Peter Matthews <peter@pjmatthews.dev>
2024-03-12 04:03:11 -0400

    update mem.Database's graph.Authenticator impls

--- mem/auth.go
+++ mem/auth.go
@@ -4,19 +4,27 @@ package mem
 import (
 	"context"
 	"errors"
-	"fmt"
 	"net/http"
 
+	"pjm.dev/sfs/graph"
 	"pjm.dev/sfs/graph/model"
 )
 
-var errMissingAuthorizationHeader = errors.New("missing Authorization header")
-
+// Authenticate determines the requesting user.
+//
+// # Arguments
+//   - r: The http request to authenticate.
+//
+// # Returns
+//   - The user who is making the request.
+//
+// # Errors
+//   - `graph.ErrUnauthorized` if an authenticated user cannot be determined.
 func (m *Database) Authenticate(r *http.Request) (model.User, error) {
 	// get name header from request
 	name := r.Header.Get("Authorization")
 	if name == "" {
-		return model.User{}, errMissingAuthorizationHeader
+		return model.User{}, graph.ErrUnauthorized
 	}
 
 	// find user by name
@@ -27,31 +35,56 @@ func (m *Database) Authenticate(r *http.Request) (model.User, error) {
 	}
 
 	// user not found
-	return model.User{}, fmt.Errorf("user %s not found", name)
+	return model.User{}, graph.ErrNotFound
 }
 
 type userContextKeyType string
 
 const userContextKey userContextKeyType = "user"
 
+// WithUser wraps a user in a context.
+//
+// # Arguments
+//   - ctx: The context to wrap the user in.
+//   - user: The user to wrap in the context.
+//
+// # Returns
+//   - A new context with the user wrapped in it.
 func (m *Database) WithUser(ctx context.Context, user model.User) context.Context {
 	return context.WithValue(ctx, userContextKey, user)
 }
 
+// FromContext extracts a user from a context.
+//
+// # Arguments
+//   - ctx: The context to extract the user from.
+//
+// # Returns
+//   - The user wrapped in the context.
+//
+// # Errors
+//   - `ErrUnauthorized` if the user is not found in the context.
 func (m *Database) FromContext(ctx context.Context) (model.User, error) {
 	user, ok := ctx.Value(userContextKey).(model.User)
 	if !ok {
-		return model.User{}, errors.New("user not found in context")
+		return model.User{}, graph.ErrUnauthorized
 	}
 
 	return user, nil
 }
 
+// WrapInAuthentication wraps a handler in an authentication layer.
+//
+// # Arguments
+//   - h: The handler to wrap in authentication.
+//
+// # Returns
+//   - Handler whose context contains the authenticated user.
 func (m *Database) WrapInAuthentication(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// authenticate user
 		user, err := m.Authenticate(r)
-		if errors.Is(err, errMissingAuthorizationHeader) {
+		if errors.Is(err, graph.ErrUnauthorized) {
 			// serve unauthenticated request
 		} else if err != nil {
 			http.Error(w, "", http.StatusUnauthorized)
