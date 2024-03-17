3f515557186f98d7405679f1f625c1d2626b8b46
Peter Matthews <peter@pjmatthews.dev>
2024-03-16 20:43:42 -0400

    codegen gql files

---
+++ app/codegen.ts
@@ -0,0 +1,14 @@
+import { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: "http://localhost:8080/graphql",
+  documents: ["src/**/*.ts"],
+  ignoreNoDocuments: true,
+  generates: {
+    "src/gql/": {
+      preset: "client",
+    },
+  },
+};
+
+export default config;
