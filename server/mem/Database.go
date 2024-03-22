07e77ca2ca527ed92ac00630563a13fb608ad25f
Peter Matthews <peter@pjmatthews.dev>
2024-03-22 07:39:23 -0400

    add doc comment to mem.Database

--- server/mem/Database.go
+++ server/mem/Database.go
@@ -5,6 +5,11 @@ import (
 	"pjm.dev/sfs/graph"
 )
 
+// Database is a simple in-memory database that implements the following interfaces:
+//
+//   - graph.SharedFileSystemer
+//   - graph.Authenticator
+//   - graph.UUIDGenerator
 type Database struct {
 	Root   *graph.Folder
 	Users  []*graph.User
