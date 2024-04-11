04e4af75c9f9c70db9adfe71186be6af2b97af05
Peter Matthews <peter.matthews@datachat.ai>
2024-04-11 12:23:03 -0700

    format schema.sql

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE "user" (
   "id" UUID NOT NULL DEFAULT gen_random_uuid(),
-  "name" VARCHAR NOT NULL,
-  PRIMARY KEY ("id")
+  PRIMARY KEY ("id"),
+  "name" VARCHAR NOT NULL
 );
 
 CREATE TABLE "node" (
   "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+  PRIMARY KEY ("id"),
   "name" VARCHAR NOT NULL,
   "owner" UUID NOT NULL,
-  "parent" UUID,
-  PRIMARY KEY ("id"),
   CONSTRAINT "node_owner" FOREIGN KEY ("owner") REFERENCES "user" ("id") ON DELETE CASCADE,
+  "parent" UUID,
   CONSTRAINT "node_parent" FOREIGN KEY ("parent") REFERENCES "node" ("id") ON DELETE CASCADE
 );
