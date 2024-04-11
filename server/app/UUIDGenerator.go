9070b04987082cd75e87056f3725b0df12754e5f
Peter Matthews <peter.matthews@datachat.ai>
2024-04-10 17:52:28 -0700

    app.app implements UUIDGenerator

---
+++ server/app/UUIDGenerator.go
@@ -0,0 +1,16 @@
+package app
+
+import "github.com/google/uuid"
+
+func (a *app) Generate() uuid.UUID {
+	var u uuid.UUID
+
+	if len(a.uuids) > 0 {
+		u = a.uuids[0]
+		a.uuids = a.uuids[1:]
+	} else {
+		u = uuid.New()
+	}
+
+	return u
+}
