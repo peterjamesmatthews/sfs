2f02f51aef12a74be7f45f2763f4cd58c1fb5e88
Peter Matthews <peter.matthews@datachat.ai>
2024-04-12 17:54:55 -0700

    update seed with new migrations

--- server/db/seed/seed.sql
+++ server/db/seed/seed.sql
@@ -25,6 +25,18 @@ CREATE SCHEMA atlas_schema_revisions;
 
 ALTER SCHEMA atlas_schema_revisions OWNER TO postgres;
 
+--
+-- Name: access_type; Type: TYPE; Schema: public; Owner: postgres
+--
+
+CREATE TYPE public.access_type AS ENUM (
+    'read',
+    'write'
+);
+
+
+ALTER TYPE public.access_type OWNER TO postgres;
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
@@ -50,6 +62,33 @@ CREATE TABLE atlas_schema_revisions.atlas_schema_revisions (
 
 
 ALTER TABLE atlas_schema_revisions.atlas_schema_revisions OWNER TO postgres;
+
+--
+-- Name: access; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.access (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    "user" uuid NOT NULL,
+    type public.access_type NOT NULL,
+    node uuid NOT NULL
+);
+
+
+ALTER TABLE public.access OWNER TO postgres;
+
+--
+-- Name: file; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.file (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    node uuid NOT NULL,
+    content text NOT NULL
+);
+
+
+ALTER TABLE public.file OWNER TO postgres;
 
 --
 -- Name: node; Type: TABLE; Schema: public; Owner: postgres
@@ -84,6 +123,25 @@ ALTER TABLE public."user" OWNER TO postgres;
 COPY atlas_schema_revisions.atlas_schema_revisions (version, description, type, applied, total, executed_at, execution_time, error, error_stmt, hash, partial_hashes, operator_version) FROM stdin;
 20240411142653	user	2	1	1	2024-04-11 21:09:21.18252+00	176666			2IUYUyAiH2Ta0sgPI0OiG+JzQLXsthKkro/hqmZb+4Q=	["h1:86EE4dgIDdHdPJm05Cz9VvG+xY8kj27yqk33U+Ky3zM="]	Atlas CLI v0.21.2-e25c033-canary
 20240411142713	node	2	1	1	2024-04-11 21:09:21.18622+00	198042			0QUkfQo+0sDgoNGvP+kSxqSN3KVmweg2S6W5r3adtvc=	["h1:vet/adCtkW/nfnqm8l+vxLq+JTtc81HNPOkT0vNN6IY="]	Atlas CLI v0.21.2-e25c033-canary
+20240413000857		2	1	1	2024-04-13 00:54:37.247834+00	308209			2hfvR3ABvTLZfmC+0610AG7bzS7T+w0/l236wUPYvAc=	["h1:HW7pHIMJ+wXOsnQtqZX3xhnrqPpBcrpC1g2uKQHg7LU="]	Atlas CLI v0.21.2-e25c033-canary
+20240413002501		2	2	2	2024-04-13 00:54:37.254697+00	244583			nwC/2Z8ibqx6ZoW1pRtaz2glXYKFFMxIq/6S2MNC59w=	["h1:hfd1OR3nfbc3NvU5qodRRUsTW2abMCh5Vv4ffnCxLKw=", "h1:e1AwpT/quUQPI8jf7nXO+/hxQbi8i52bh/o8B2/zYYc="]	Atlas CLI v0.21.2-e25c033-canary
+20240413004158		2	2	2	2024-04-13 00:54:37.258228+00	164083			HR0LrbSMGjrmNB3sQQJ07BlhzhUzm0IStuHEzuQIdUU=	["h1:N7sxtFcgk6B9LQnKQgfw158pek6coSw2NpgUEyzX1i0=", "h1:57JLXVi+rdcvMbAUVHmMR7ydSfWMKM60OqSFrKnPfNQ="]	Atlas CLI v0.21.2-e25c033-canary
+\.
+
+
+--
+-- Data for Name: access; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.access (id, "user", type, node) FROM stdin;
+\.
+
+
+--
+-- Data for Name: file; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.file (id, node, content) FROM stdin;
 \.
 
 
@@ -112,6 +170,22 @@ ALTER TABLE ONLY atlas_schema_revisions.atlas_schema_revisions
 
 
 --
+-- Name: access access_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.access
+    ADD CONSTRAINT access_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: file file_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.file
+    ADD CONSTRAINT file_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: node node_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -125,6 +199,44 @@ ALTER TABLE ONLY public.node
 
 ALTER TABLE ONLY public."user"
     ADD CONSTRAINT user_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: access_user_type_node; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX access_user_type_node ON public.access USING btree ("user", type, node);
+
+
+--
+-- Name: node_name_owner_parent; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX node_name_owner_parent ON public.node USING btree (name, owner, parent);
+
+
+--
+-- Name: access access_node; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.access
+    ADD CONSTRAINT access_node FOREIGN KEY (node) REFERENCES public.node(id) ON DELETE CASCADE;
+
+
+--
+-- Name: access access_user; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.access
+    ADD CONSTRAINT access_user FOREIGN KEY ("user") REFERENCES public."user"(id) ON DELETE CASCADE;
+
+
+--
+-- Name: file file_node; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.file
+    ADD CONSTRAINT file_node FOREIGN KEY (node) REFERENCES public.node(id) ON DELETE CASCADE;
 
 
 --
