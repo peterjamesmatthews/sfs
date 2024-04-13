543b78e7a3ba3e97a7c2e5f8af2e6794100ef6a3
Peter Matthews <peter.matthews@datachat.ai>
2024-04-12 17:28:12 -0700

    add access_type enum and access table

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -22,3 +22,14 @@ CREATE TABLE "file" (
   "content" TEXT NOT NULL
 );
 
+CREATE TYPE "access_type" AS ENUM ('read', 'write');
+
+CREATE TABLE "access" (
+  "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+  PRIMARY KEY ("id"),
+  "user" UUID NOT NULL,
+  CONSTRAINT "access_user" FOREIGN KEY ("user") REFERENCES "user" ("id") ON DELETE CASCADE,
+  "type" "access_type" NOT NULL,
+  "node" UUID NOT NULL,
+  CONSTRAINT "access_node" FOREIGN KEY ("node") REFERENCES "node" ("id") ON DELETE CASCADE
+);
