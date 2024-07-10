a1c48bee6c9f5f0afd2516e3379ec99093347cc4
pjm <peter@pjmatthews.dev>
2024-07-09 20:23:45 -0400

    parametrize TestGetTokensFromAuth0; add existing user test case

--- server/server/auth_test.go
+++ server/server/auth_test.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -18,7 +19,7 @@ func (m *mock) GetIDAndEmailFromToken(token string) (string, string, error) {
 }
 
 // TestGetTokensFromAuth0 tests server's handling of the getTokensFromAuth0 query.
-func TestGetTokensFromAuth0(t *testing.T) { // TODO parametrize test with more cases
+func TestGetTokensFromAuth0(t *testing.T) {
 	server, mock, db := newTestServer(t)
 	defer server.Close()
 
@@ -35,34 +36,62 @@ func TestGetTokensFromAuth0(t *testing.T) { // TODO parametrize test with more c
 		t.Fatalf("failed to parse server URL: %v", err)
 	}
 
-	request := &http.Request{
-		Method: http.MethodPost,
-		URL:    url,
-		Header: http.Header{"Content-Type": []string{"application/json"}},
-		Body:   io.NopCloser(strings.NewReader(fmt.Sprintf(`{"query":"query { getTokensFromAuth0(token: \"%s\") { access refresh } }"}`, token))), // TODO ref to test fixture
+	tests := []test{
+		{
+			name: "creates new user",
+			request: &http.Request{
+				Method: http.MethodPost,
+				URL:    url,
+				Header: http.Header{"Content-Type": []string{"application/json"}},
+				Body:   io.NopCloser(strings.NewReader(fmt.Sprintf(`{"query":"query { getTokensFromAuth0(token: \"%s\") { access refresh } }"}`, token))), // TODO ref to test fixture
+			},
+			response: &http.Response{StatusCode: http.StatusOK},
+		},
+		{
+			name: "authenticates existing user",
+			request: &http.Request{
+				Method: http.MethodPost,
+				URL:    url,
+				Header: http.Header{"Content-Type": []string{"application/json"}},
+				Body:   io.NopCloser(strings.NewReader(fmt.Sprintf(`{"query":"query { getTokensFromAuth0(token: \"%s\") { access refresh } }"}`, token))), // TODO ref to test fixture
+			},
+			response: &http.Response{StatusCode: http.StatusOK},
+		},
 	}
-	want := &http.Response{StatusCode: http.StatusOK}
 
-	got, err := server.Client().Do(request)
-	if err != nil {
-		t.Fatalf("failed to send request: %v", err)
+	for _, test := range tests {
+		got, err := server.Client().Do(test.request)
+		if err != nil {
+			t.Fatalf("failed to send request: %v", err)
+		}
+
+		assert.Equal(
+			t, test.response.StatusCode, got.StatusCode,
+			"unexpected status code: want %d, got %d", test.response.StatusCode, got.StatusCode,
+		)
+
+		bytes, err := io.ReadAll(got.Body)
+		if err != nil {
+			t.Fatalf("failed to read response body: %v", err)
+		}
+		body := string(bytes)
+
+		assert.NotContains(t, body, "errors", "unexpected errors in response: %s", body)
+		assert.Contains(t, body, "access", "access token missing from response: %s", body)
+		assert.Contains(t, body, "refresh", "refresh token missing from response: %s", body)
+		mock.AssertExpectations(t)
+
+		dump := dumpDatabase(db, t)
+		if err != nil {
+			t.Fatalf("failed to dump database: %v", err)
+		}
+
+		assert.Contains(t, dump, id, "user ID missing from database dump: %s", dump)
+		assert.Contains(t, dump, email, "user email missing from database dump: %s", dump)
 	}
+}
 
-	assert.Equal(t, want.StatusCode, got.StatusCode, "unexpected status code: want %d, got %d", want.StatusCode, got.StatusCode)
-
-	bytes, err := io.ReadAll(got.Body)
-	if err != nil {
-		t.Fatalf("failed to read response body: %v", err)
-	}
-	body := string(bytes)
-
-	assert.NotContains(t, body, "errors", "unexpected errors in response: %s", body)
-	assert.Contains(t, body, "access", "access token missing from response: %s", body)
-	assert.Contains(t, body, "refresh", "refresh token missing from response: %s", body)
-	mock.AssertExpectations(t)
-
-	var dump string
-
+func dumpDatabase(db *pgx.Conn, t *testing.T) string {
 	cmd := exec.Command(
 		"pg_dump",
 		"-d", db.Config().Database,
@@ -75,8 +104,5 @@ func TestGetTokensFromAuth0(t *testing.T) { // TODO parametrize test with more c
 	if err != nil {
 		t.Fatalf("failed to dump database: %v", err)
 	}
-	dump = string(output)
-
-	assert.Contains(t, dump, id, "user ID missing from database dump: %s", dump)
-	assert.Contains(t, dump, email, "user email missing from database dump: %s", dump)
+	return string(output)
 }
