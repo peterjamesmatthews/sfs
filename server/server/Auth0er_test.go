495164e7a181e555575567551ba543c19271772d
pjm <peter@pjmatthews.dev>
2024-07-10 23:54:43 -0400

    mv mock's app.Auth0er impl to its own file

---
+++ server/server/Auth0er_test.go
@@ -0,0 +1,8 @@
+package server_test
+
+// this file provides mock's implementation of the app.Auth0er interface.
+
+func (m *mock) GetIDAndEmailFromToken(token string) (string, string, error) {
+	args := m.Called(token)
+	return args.String(0), args.String(1), args.Error(2)
+}
--- server/server/auth_test.go
+++ server/server/auth_test.go
@@ -11,11 +11,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func (m *mock) GetIDAndEmailFromToken(token string) (string, string, error) {
-	args := m.Called(token)
-	return args.String(0), args.String(1), args.Error(2)
-}
-
 // TestGetTokensFromAuth0 tests server's handling of the getTokensFromAuth0 query.
 func TestGetTokensFromAuth0(t *testing.T) {
 	server, mock, db := newTestServer(t)
