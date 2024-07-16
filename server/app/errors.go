6cdc6d9e42c874ede24b996c62a24e10e6cb40a4
pjm <peter@pjmatthews.dev>
2024-07-15 23:03:39 -0500

    rm unused pg error files

--- server/app/errors.go
+++ server/app/errors.go
@@ -4,14 +4,7 @@ import (
 	"errors"
 
 	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgconn"
-	"pjm.dev/sfs/db/models"
 )
-
-func (a *App) isConflictError(err error) bool {
-	var pgErr *pgconn.PgError
-	return errors.As(err, &pgErr) && pgErr.Code == models.UniqueViolation
-}
 
 func (a *App) isNotFoundError(err error) bool {
 	return errors.Is(err, pgx.ErrNoRows)
