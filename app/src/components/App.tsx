6dce35602e354a455d778b33f21f354347d1dd21
pjm <peter@pjmatthews.dev>
2024-07-15 23:03:19 -0500

    schedule refresh when access token exp

--- app/src/components/App.tsx
+++ app/src/components/App.tsx
@@ -4,23 +4,18 @@ import { useSelector } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import router from "../router";
 import store from "../store";
-import {
-  refreshTokens,
-  selectAccessToken,
-  selectRefreshToken,
-} from "../store/slices/auth";
+import { refreshTokens, selectAccessToken } from "../store/slices/auth";
 import SignIn from "./SignIn";
 
 export default function App() {
   const accessToken = useSelector(selectAccessToken);
-  const refreshToken = useSelector(selectRefreshToken);
 
   useEffect(() => {
-    if (!refreshToken) return;
-    const { exp } = jwtDecode(refreshToken);
+    if (!accessToken) return;
+    const { exp } = jwtDecode(accessToken);
     if (!exp) return;
     store.dispatch(refreshTokens({ expiresAt: new Date(exp * 1000) }));
-  }, [refreshToken]);
+  }, [accessToken]);
 
   if (!accessToken) return <SignIn />;
   return <RouterProvider router={router} />;
